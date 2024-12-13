2b67d1258cb69154dd5b8a0f850b553b2475b201
sarthak <mrsarthakgupta@gmail.com>
2024-12-13 18:52:51 +0530

    exit removed

--- SpatialAnalysisAgent/SpatialAnalysisAgent_DataEye/data_eye.py
+++ SpatialAnalysisAgent/SpatialAnalysisAgent_DataEye/data_eye.py
@@ -78,9 +78,6 @@ def add_data_overview_to_data_location(task, data_location_list, model = r'gpt-4
     response = get_LLM_reply(prompt=prompt)
     # pprint.pp(result.choices[0].message)
     
-    print("prompt:", prompt)
-    print("response:", response)
-    exit()
     attributes_json = json.loads(response)
     get_data_overview(attributes_json)
 
